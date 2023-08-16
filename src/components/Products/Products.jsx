d281e9e063abf29c95234465aa0396bcaf07cd84
pafz <paferza@gmail.com>
2023-08-16 16:19:39 +0200

    Products component without disable price

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -1,21 +1,14 @@
 import React, { useContext, useEffect, useState } from 'react';
 import { ProductsContext } from '../../context/ProductsContext/ProductsState';
 import { Link } from 'react-router-dom';
-import { Space, Input } from 'antd';
-import { Slider, Switch } from 'antd';
+import { Space, Input, Slider, Switch, Card } from 'antd';
 
 const { Search } = Input;
 
 const Products = () => {
-  //if searchTerm is empty -> not search
+  //TODO: button to findAll again after search???
   const { getProducts, products, addCart, filters, setFilters } =
     useContext(ProductsContext);
-
-  const [disabled, setDisabled] = useState(false);
-
-  const onChange = checked => {
-    setDisabled(checked);
-  };
 
   useEffect(() => {
     getProducts(filters);
@@ -52,12 +45,10 @@ const Products = () => {
         }}
         max={40}
         defaultValue={[1, 10]}
-        disabled={disabled}
         onAfterChange={([low, high]) => {
           setFilters({ low, high });
         }}
       />
-      Disabled: <Switch size="small" checked={disabled} onChange={onChange} />
       <div>{product}</div>
     </>
   );
