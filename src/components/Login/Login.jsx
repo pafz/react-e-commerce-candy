04fb7dcc4ab5ae9bfc8a951e620ad87c9b6442a3
pafz <paferza@gmail.com>
2023-08-21 11:43:07 +0200

    form centered

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -27,40 +27,44 @@ const Login = () => {
   }, [login]);
 
   return (
-    <div className="container">
-      <Form
-        name="basic"
-        labelCol={{ span: 8 }}
-        wrapperCol={{ span: 16 }}
-        initialValues={{ remember: true }}
-        onFinish={onFinish}
-        onFinishFailed={onFinishFailed}
-        autoComplete="off"
-      >
-        <Form.Item
-          label="Email"
-          name="mail"
-          rules={[{ required: true, message: 'Please input your email!' }]}
+    <>
+      <h1>Welcome sweet tooth</h1>
+      <div className="container">
+        <Form
+          name="basic"
+          labelCol={{ span: 6 }}
+          wrapperCol={{ span: 14 }}
+          initialValues={{ remember: true }}
+          onFinish={onFinish}
+          onFinishFailed={onFinishFailed}
+          autoComplete="off"
         >
-          <Input />
-        </Form.Item>
+          <Form.Item
+            label="mail"
+            name="mail"
+            rules={[{ required: true, message: 'Please input your mail!' }]}
+          >
+            <Input />
+          </Form.Item>
 
-        <Form.Item
-          label="Password"
-          name="password"
-          rules={[{ required: true, message: 'Please input your password!' }]}
-        >
-          <Input.Password />
-        </Form.Item>
+          <Form.Item
+            label="password"
+            name="password"
+            rules={[{ required: true, message: 'Please input your password!' }]}
+          >
+            <Input.Password />
+          </Form.Item>
 
-        <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
-          <Button type="primary" htmlType="submit">
-            Submit
-          </Button>
-        </Form.Item>
-      </Form>
-    </div>
+          <Form.Item wrapperCol={{ offset: 8, span: 10 }}>
+            <Button type="primary" htmlType="submit">
+              Submit
+            </Button>
+          </Form.Item>
+        </Form>
+      </div>
+    </>
   );
 };
 
 export default Login;
+//https://ant.design/components/form
